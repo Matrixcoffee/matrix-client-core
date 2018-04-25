e18a3d7d2217cb7f2763f8b40831a37e358cee5d
Matrixcoffee <Matrixcoffee@users.noreply.github.com>
2018-04-25 08:58:04 +0000

    Pass '-i' flag to python in testclient.sh

--- testclient.sh
+++ testclient.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-PYTHONPATH=.:../urllib-requests-adapter:../matrix-python-sdk python3 TestClient.py "$@"
+PYTHONFLAGS=
+
+while [ "$1" ]; do case "$1" in
+	-i)	PYTHONFLAGS="$PYTHONFLAGS $1"
+		shift
+		;;
+	--)	shift
+		break
+		;;
+	*)	break
+		;;
+esac; done
+
+PYTHONPATH=.:../urllib-requests-adapter:../matrix-python-sdk python3 $PYTHONFLAGS TestClient.py "$@"
